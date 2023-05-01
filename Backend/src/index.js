cf0816d2c6b702789c4eb93f90562a945ac60e1a
Houda20-bh <bh.houda14@gmail.com>
2023-05-01 03:11:35 +0100

    add my last updates

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,5 +1,11 @@
 const express = require('express')
+const dotenv= require('dotenv')
+const morgan =require('morgan')
+const cors = require('cors')
+dotenv.config();
 const app = express();
+app.use(morgan('tiny'));
+app.use(cors());
 //TODO: configure the app
 app.get('/todos', (req,res)=>{
     res.send('Hello there back again:Words are free. It is how you use them that may cost you ')
